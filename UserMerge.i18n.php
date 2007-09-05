0269264f58f2a56e35a0c1e3b291244aec0c19df
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-05 23:01:45 +0000

    Updating/adding nl messages for extensions UserMerge and MiniPreview

--- UserMerge.i18n.php
+++ UserMerge.i18n.php
@@ -23,6 +23,18 @@ $allMessages = array(
                 'usermerge' => 'Benutzerkonten zusammenführen und löschen'
         ),
         'nl' => array( 
-                'usermerge' => 'Bewerkingen gebruikers samenvoegen en verwijderen'
+                'usermerge' => 'Gebruikers samenvoegen en verwijderen'
+				'usermerge-badolduser' => 'Verkeerde oude gebruiker',
+				'usermerge-badnewuser' => 'Verkeerde nieuwe gebruiker',
+				'usermerge-nonewuser' => 'Nieuwe gebruiker is niet ingegeven - er wordt aangenomen dat er samengevoegd moet worden naar $1.<br />Klik <u>Gebruiker samenvoegen</u> om te accepteren.',
+				'usermerge-noolduser' => 'Oude gebruiker is niet ingegeven',
+				'usermerge-olduser' => 'Oude gebruiker (samenvoegen van)',
+				'usermerge-newuser' => 'Nieuwe gebruiker (samenvoegen naar)',
+				'usermerge-deleteolduser' => 'Verwijder oude gebruiker?',
+				'usermerge-submit' => 'Gebruiker samenvoegen',
+				'usermerge-badtoken' => 'Ongeldig bewerkingstoken',
+				'usermerge-userdeleted' => '$1($2) is verwijderd.',
+				'usermerge-updating' => 'Tabel $1 aan het bijwerken ($2 naar $3)',
+				'usermerge-success' => 'Samenvoegen van $1($2) naar $3($4) is afgerond.'
         ),
 );
