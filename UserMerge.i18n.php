4bb8985301d4c1fbd6e5c2b5fdd960e4b41f85fb
Niklas Laxström <nikerabbit@users.mediawiki.org>
2007-09-07 07:10:19 +0000

    * Fix syntax error and space/tab mess

--- UserMerge.i18n.php
+++ UserMerge.i18n.php
@@ -4,37 +4,37 @@
 */
 
 $allMessages = array(
-        'en' => array( 
-                'usermerge' => 'User Merge and Delete', 
-				'usermerge-badolduser' => 'Invalid Old Username',
-				'usermerge-badnewuser' => 'Invalid New User',
-				'usermerge-nonewuser' => 'Empty New Username - Assuming merge to $1.<br />Click <u>Merge User</u> to accept.',
-				'usermerge-noolduser' => 'Empty Old Username',
-				'usermerge-olduser' => 'Old User(Merge From)',
-				'usermerge-newuser' => 'New User(Merge To)',
-				'usermerge-deleteolduser' => 'Delete Old User?',
-				'usermerge-submit' => 'Merge User',
-				'usermerge-badtoken' => 'Invalid Edit Token',
-				'usermerge-userdeleted' => '$1($2) has been deleted.',
-				'usermerge-updating' => 'Updating $1 table ($2 to $3)',
-				'usermerge-success' => 'Merge from $1($2) to $3($4) is complete.'
-        ),
-        'de' => array( 
-                'usermerge' => 'Benutzerkonten zusammenführen und löschen'
-        ),
-        'nl' => array( 
-                'usermerge' => 'Gebruikers samenvoegen en verwijderen'
-				'usermerge-badolduser' => 'Verkeerde oude gebruiker',
-				'usermerge-badnewuser' => 'Verkeerde nieuwe gebruiker',
-				'usermerge-nonewuser' => 'Nieuwe gebruiker is niet ingegeven - er wordt aangenomen dat er samengevoegd moet worden naar $1.<br />Klik <u>Gebruiker samenvoegen</u> om te accepteren.',
-				'usermerge-noolduser' => 'Oude gebruiker is niet ingegeven',
-				'usermerge-olduser' => 'Oude gebruiker (samenvoegen van)',
-				'usermerge-newuser' => 'Nieuwe gebruiker (samenvoegen naar)',
-				'usermerge-deleteolduser' => 'Verwijder oude gebruiker?',
-				'usermerge-submit' => 'Gebruiker samenvoegen',
-				'usermerge-badtoken' => 'Ongeldig bewerkingstoken',
-				'usermerge-userdeleted' => '$1($2) is verwijderd.',
-				'usermerge-updating' => 'Tabel $1 aan het bijwerken ($2 naar $3)',
-				'usermerge-success' => 'Samenvoegen van $1($2) naar $3($4) is afgerond.'
-        ),
+'en' => array(
+	'usermerge' => 'User Merge and Delete',
+	'usermerge-badolduser' => 'Invalid Old Username',
+	'usermerge-badnewuser' => 'Invalid New User',
+	'usermerge-nonewuser' => 'Empty New Username - Assuming merge to $1.<br />Click <u>Merge User</u> to accept.',
+	'usermerge-noolduser' => 'Empty Old Username',
+	'usermerge-olduser' => 'Old User(Merge From)',
+	'usermerge-newuser' => 'New User(Merge To)',
+	'usermerge-deleteolduser' => 'Delete Old User?',
+	'usermerge-submit' => 'Merge User',
+	'usermerge-badtoken' => 'Invalid Edit Token',
+	'usermerge-userdeleted' => '$1($2) has been deleted.',
+	'usermerge-updating' => 'Updating $1 table ($2 to $3)',
+	'usermerge-success' => 'Merge from $1($2) to $3($4) is complete.'
+),
+'de' => array(
+	'usermerge' => 'Benutzerkonten zusammenführen und löschen',
+),
+'nl' => array(
+	'usermerge' => 'Gebruikers samenvoegen en verwijderen',
+	'usermerge-badolduser' => 'Verkeerde oude gebruiker',
+	'usermerge-badnewuser' => 'Verkeerde nieuwe gebruiker',
+	'usermerge-nonewuser' => 'Nieuwe gebruiker is niet ingegeven - er wordt aangenomen dat er samengevoegd moet worden naar $1.<br />Klik <u>Gebruiker samenvoegen</u> om te accepteren.',
+	'usermerge-noolduser' => 'Oude gebruiker is niet ingegeven',
+	'usermerge-olduser' => 'Oude gebruiker (samenvoegen van)',
+	'usermerge-newuser' => 'Nieuwe gebruiker (samenvoegen naar)',
+	'usermerge-deleteolduser' => 'Verwijder oude gebruiker?',
+	'usermerge-submit' => 'Gebruiker samenvoegen',
+	'usermerge-badtoken' => 'Ongeldig bewerkingstoken',
+	'usermerge-userdeleted' => '$1($2) is verwijderd.',
+	'usermerge-updating' => 'Tabel $1 aan het bijwerken ($2 naar $3)',
+	'usermerge-success' => 'Samenvoegen van $1($2) naar $3($4) is afgerond.'
+),
 );
