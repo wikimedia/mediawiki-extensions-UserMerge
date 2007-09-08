6659f9019132c424cbffa33a3c902014173bcc73
Raimond Spekking <raymond@users.mediawiki.org>
2007-09-08 04:39:31 +0000

    * (bug 11230) Update Chinese translations   Patches by Shinjiman

--- UserMerge.i18n.php
+++ UserMerge.i18n.php
@@ -37,4 +37,56 @@ $allMessages = array(
 	'usermerge-updating' => 'Tabel $1 aan het bijwerken ($2 naar $3)',
 	'usermerge-success' => 'Samenvoegen van $1($2) naar $3($4) is afgerond.'
 ),
+'yue' => array(
+	'usermerge' => '用戶合併同刪除',
+	'usermerge-badolduser' => '無效嘅舊用戶名',
+	'usermerge-badnewuser' => '無效嘅新用戶名',
+	'usermerge-nonewuser' => '清除新用戶名 - 假設合併到$1。<br />撳<u>合併用戶</u>去接受。',
+	'usermerge-noolduser' => '清除舊用戶名',
+	'usermerge-olduser' => '舊用戶 (合併自)',
+	'usermerge-newuser' => '新用戶 (合併到)',
+	'usermerge-deleteolduser' => '刪舊用戶？',
+	'usermerge-submit' => '合併用戶',
+	'usermerge-badtoken' => '無效嘅編輯幣',
+	'usermerge-userdeleted' => '$1($2) 已經刪除咗。',
+	'usermerge-updating' => '更新緊 $1 表 ($2 到 $3)',
+	'usermerge-success' => '由 $1($2) 到 $3($4) 嘅合併已經完成。'
+),
+'zh-hans' => array(
+	'usermerge' => '用户合并和删除',
+	'usermerge-badolduser' => '无效的旧用户名',
+	'usermerge-badnewuser' => '无效的新用户名',
+	'usermerge-nonewuser' => '清除新用户名 - 假设合并到$1。<br />点击<u>合并用户</u>以接受。',
+	'usermerge-noolduser' => '清除旧用户名',
+	'usermerge-olduser' => '旧用户 (合并自)',
+	'usermerge-newuser' => '新用户 (合并到)',
+	'usermerge-deleteolduser' => '删除旧用户？',
+	'usermerge-submit' => '合并用户',
+	'usermerge-badtoken' => '无效的编辑币',
+	'usermerge-userdeleted' => '$1($2) 已删除。',
+	'usermerge-updating' => '正在更新 $1 表格 ($2 到 $3)',
+	'usermerge-success' => '由 $1($2) 到 $3($4) 的合并已经完成。'
+),
+'zh-hant' => array(
+	'usermerge' => '用戶合併和刪除',
+	'usermerge-badolduser' => '無效的舊用戶名',
+	'usermerge-badnewuser' => '無效的新用戶名',
+	'usermerge-nonewuser' => '清除新用戶名 - 假設合併到$1。<br />點擊<u>合併用戶</u>以接受。',
+	'usermerge-noolduser' => '清除舊用戶名',
+	'usermerge-olduser' => '舊用戶 (合併自)',
+	'usermerge-newuser' => '新用戶 (合併到)',
+	'usermerge-deleteolduser' => '刪除舊用戶？',
+	'usermerge-submit' => '合併用戶',
+	'usermerge-badtoken' => '無效的編輯幣',
+	'usermerge-userdeleted' => '$1($2) 已刪除。',
+	'usermerge-updating' => '正在更新 $1 表格 ($2 到 $3)',
+	'usermerge-success' => '由 $1($2) 到 $3($4) 的合併已經完成。'
+),
 );
+
+$allMessages['zh'] = $allMessages['zh-hans'];
+$allMessages['zh-cn'] = $allMessages['zh-hans'];
+$allMessages['zh-hk'] = $allMessages['zh-hant'];
+$allMessages['zh-sg'] = $allMessages['zh-hans'];
+$allMessages['zh-tw'] = $allMessages['zh-hant'];
+$allMessages['zh-yue'] = $allMessages['yue'];
