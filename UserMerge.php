868824a2f131215b4a0461e07ddc3dc6397c63bd
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-10 23:04:24 +0000

    * complete $wgExtensionCredits * remove trailing whitespace

--- UserMerge.php
+++ UserMerge.php
@@ -11,11 +11,11 @@ if (!defined('MEDIAWIKI')) {
 
 $wgExtensionFunctions[] = 'efUserMerge';
 $wgExtensionCredits['specialpage'][] = array(
-    'name'=>'User Merge and Delete',
-    'url'=>'http://www.mediawiki.org/wiki/Extension:User_Merge_and_Delete',
-    'author'=>'Tim Laqua',
-    'description'=>"Merges references from one user to another user in the Wiki database - will also delete old users following merge.  Requires 'usermerge' privileges.",
-    'version'=>'1.3'
+	'name'=>'User Merge and Delete',
+	'url'=>'http://www.mediawiki.org/wiki/Extension:User_Merge_and_Delete',
+	'author'=>'Tim Laqua',
+	'description'=>"Merges references from one user to another user in the Wiki database - will also delete old users following merge.  Requires 'usermerge' privileges",
+	'version'=>'1.3',
 );
 
 $wgAutoloadClasses['UserMerge'] = dirname(__FILE__) . '/UserMerge_body.php';
@@ -29,7 +29,7 @@ function efUserMerge() {
 	foreach( $usermergeMessages as $key => $value ) {
 		$wgMessageCache->addMessages( $usermergeMessages[$key], $key );
 	}
-	
+
 	# Add a new log type
 	global $wgLogTypes, $wgLogNames, $wgLogHeaders, $wgLogActions;
 	$wgLogTypes[]                 		= 'usermerge';
@@ -38,4 +38,3 @@ function efUserMerge() {
 	$wgLogActions['usermerge/mergeuser'] 	= 'usermerge-success-log';
 	$wgLogActions['usermerge/deleteuser']	= 'usermerge-userdeleted-log';
 }
-
