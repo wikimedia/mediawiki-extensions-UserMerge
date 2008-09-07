7fd5197221388a51394c6d3e10f78f5fb152cd8a
Meno25 <mfarag@users.mediawiki.org>
2008-09-07 09:07:05 +0000

    Add author info

--- UserMerge.alias.php
+++ UserMerge.alias.php
@@ -10,12 +10,16 @@ $aliases['en'] = array(
 	'UserMerge' => array( 'UserMerge' ),
 );
 
-/** Arabic (العربية) */
+/** Arabic (العربية)
+ * @author Meno25
+ */
 $aliases['ar'] = array(
 	'UserMerge' => array( 'دمج_مستخدم' ),
 );
 
-/** Egyptian Spoken Arabic (مصرى) */
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
 $aliases['arz'] = array(
 	'UserMerge' => array( 'دمج_مستخدم' ),
 );
