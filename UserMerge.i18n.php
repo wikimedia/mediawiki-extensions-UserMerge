6b0f8a6ecfa36d3b2107f51a422574cdc41db2cd
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-02-07 22:04:21 +0000

    Localisation updates for extensions messages from translatewiki.net (2009-02-07 21:24 UTC)

--- UserMerge.i18n.php
+++ UserMerge.i18n.php
@@ -1101,6 +1101,7 @@ $messages['ps'] = array(
  * @author Lijealso
  * @author Malafaya
  * @author Sir Lestaty de Lioncourt
+ * @author Waldir
  */
 $messages['pt'] = array(
 	'usermerge' => 'Fusão e eliminação de utilizadores',
@@ -1113,13 +1114,13 @@ Clique <u>Fundir Utilizador</u> para aceitar.',
 	'usermerge-olduser' => 'Utilizador antigo (fundir de)',
 	'usermerge-newuser' => 'Utilizador novo (fundir para)',
 	'usermerge-deleteolduser' => 'Apagar utilizador antigo?',
-	'usermerge-submit' => 'Limpar usuário',
+	'usermerge-submit' => 'Fundir utilizador',
 	'usermerge-badtoken' => 'Ficha de edição inválida',
 	'usermerge-userdeleted' => '$1 ($2) foi eliminado.',
-	'usermerge-userdeleted-log' => 'Usuário eliminado: $2 ($3)',
+	'usermerge-userdeleted-log' => 'Utilizador eliminado: $2 ($3)',
 	'usermerge-updating' => 'Actualizando tabela $1 ($2 para $3)',
 	'usermerge-success' => 'Fusão de $1 ($2) para $3 ($4) está completa.',
-	'usermerge-success-log' => 'Usuário $2 ($3) fundido com $4 ($5)',
+	'usermerge-success-log' => 'Utilizador $2 ($3) fundido com $4 ($5)',
 	'usermerge-logpage' => 'Registo de fusão de utilizadores',
 	'usermerge-logpagetext' => 'Este é um registo de acções de fusão de utilizadores.',
 	'usermerge-noselfdelete' => 'Você não pode apagar ou fundir a partir de si próprio!',
