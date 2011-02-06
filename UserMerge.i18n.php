c3012eaada914dbe545508dbd26e54164c9c24b9
Raimond Spekking <raymond@users.mediawiki.org>
2011-02-06 21:25:59 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-02-06 21:10:00 UTC)

--- UserMerge.i18n.php
+++ UserMerge.i18n.php
@@ -1738,6 +1738,7 @@ Kliknite »{{int:usermerge-submit}}« za sprejetje.',
 
 /** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬)
  * @author Rancher
+ * @author Жељко Тодоровић
  * @author Михајло Анђелковић
  */
 $messages['sr-ec'] = array(
@@ -1756,7 +1757,7 @@ $messages['sr-ec'] = array(
 	'usermerge-success' => 'Спајање $1 ($2) са $3 ($4) је завршено.',
 	'usermerge-success-log' => 'Корисник $2 ($3) је спојен са $4 ($5)',
 	'usermerge-logpage' => 'Историја спајања корисника',
-	'usermerge-logpagetext' => 'Ово је историјат спајања корисника.',
+	'usermerge-logpagetext' => 'Ово је историја спајања корисника.',
 	'usermerge-noselfdelete' => 'Не можете да се обришете или спојите са другим налогом!',
 	'usermerge-protectedgroup' => 'Није могуђе спојити овог корисника са другим — налази се у заштићеној групи.',
 	'right-usermerge' => 'Споји кориснике',
