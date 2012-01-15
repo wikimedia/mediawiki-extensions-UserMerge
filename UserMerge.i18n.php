18374aff7213c30e32911c2ff463d068336a4d34
Raimond Spekking <raymond@users.mediawiki.org>
2012-01-15 20:22:43 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- UserMerge.i18n.php
+++ UserMerge.i18n.php
@@ -329,6 +329,7 @@ Klikañ war "{{int:usermerge-submit}}" evit asantiñ.',
 	'usermerge-unmergable' => 'Dibosupl kendeuziñ adal un implijer - un niv. anaout pe un anv bet termenet evel digendeuzadus.',
 	'usermerge-protectedgroup' => 'Dibosupl kendeuziñ an implijer - emañ-eñ en ur strollad gwarezet',
 	'right-usermerge' => 'Kendeuziñ implijerien',
+	'usermerge-page-deleted' => 'Pajenn $1 diverket',
 );
 
 /** Bosnian (Bosanski)
