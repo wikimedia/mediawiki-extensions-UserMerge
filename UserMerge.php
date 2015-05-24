42d6c27d747cf14bd64efdbec6d1c3883ef6f59c
Lewis Cawte <lewis@lewiscawte.me>
2015-05-24 17:30:00 +0200

    Convert to extension registration
    Bug: T87989
    Change-Id: I08c992458964b0e54f82d0cc46e7632037510ddf

--- UserMerge.php
+++ UserMerge.php
@@ -1,80 +1,14 @@
 <?php
-/** \file
- * \brief Contains setup code for the User Merge and Delete Extension.
- */
-
-/**
- * UserMerge Extension for MediaWiki
- *
- * Copyright (C) Tim Laqua
- * Copyright (C) Thomas Gries
- * Copyright (C) Matthew April
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
- */
-
-# Not a valid entry point, skip unless MEDIAWIKI is defined
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'UserMerge' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['UserMerge'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['UserMergeAlias'] = __DIR__ . '/UserMerge.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for UserMerge extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the UserMerge extension requires MediaWiki 1.25+' );
 }
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'UserMerge',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:UserMerge',
-	'author'         => array( 'Tim Laqua', 'Thomas Gries', 'Matthew April' ),
-	'descriptionmsg' => 'usermerge-desc',
-	'version'        => '1.9.0',
-	'license-name'   => 'GPL-2.0+'
-);
-
-// Configuration options:
-
-/**
- * Whether to allow users to delete other users.
- * If false, users with the "usermerge" right
- * will only be able to merge other users.
- *
- * @var bool
- */
-$wgUserMergeEnableDelete = true;
-
-
-$wgAvailableRights[] = 'usermerge';
-# $wgGroupPermissions['bureaucrat']['usermerge'] = true;
-
-$dir = __DIR__ . '/';
-$wgAutoloadClasses['SpecialUserMerge'] = $dir . 'SpecialUserMerge.php';
-$wgAutoloadClasses['UserMerge'] = $dir . 'SpecialUserMerge.php'; // back-compat
-$wgAutoloadClasses['MergeUser'] = $dir . 'MergeUser.php';
-$wgAutoloadClasses['IUserMergeLogger'] = $dir . 'IUserMergeLogger.php';
-$wgAutoloadClasses['UserMergeLogger'] = $dir . 'UserMergeLogger.php';
-$wgAutoloadClasses['UserMergeHooks'] = $dir . 'UserMerge.hooks.php';
-
-$wgHooks['UnitTestsList'][] = 'UserMergeHooks::onUnitTestsList';
-
-$wgMessagesDirs['UserMerge'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['UserMergeAlias'] = $dir . 'UserMerge.alias.php';
-$wgSpecialPages['UserMerge'] = 'UserMerge';
-
-$wgUserMergeProtectedGroups = array( "sysop" );
-
-# Add a new log type
-$wgLogTypes[]                         = 'usermerge';
-$wgLogNames['usermerge']              = 'usermerge-logpage';
-$wgLogHeaders['usermerge']            = 'usermerge-logpagetext';
-$wgLogActions['usermerge/mergeuser']  = 'usermerge-success-log';
-$wgLogActions['usermerge/deleteuser'] = 'usermerge-userdeleted-log';
