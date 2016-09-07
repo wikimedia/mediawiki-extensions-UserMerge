ff7ce2cdcc3ae96f954956c0a8e7f654cccb8f63
Aaron Schulz <aschulz@wikimedia.org>
2016-09-06 17:41:57 -0700

    Cleanup LBFactory code and use commitAndWaitForReplication()
    Change-Id: Iec6dd9493447ef7d1ef26a7e0c8f483a105cf192

--- MergeUser.php
+++ MergeUser.php
@@ -1,4 +1,5 @@
 <?php
+use MediaWiki\MediaWikiServices;
 
 /**
  * Contains the actual database backend logic for merging users
@@ -223,7 +224,8 @@ class MergeUser {
 		Hooks::run( 'UserMergeAccountFields', [ &$updateFields ] );
 
 		$dbw = wfGetDB( DB_MASTER );
-		$lbFactory = wfGetLBFactory();
+		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
+		$ticket = $lbFactory->getEmptyTransactionTicket( __METHOD__ );
 
 		$this->deduplicateWatchlistEntries( $dbw );
 		$this->mergeBlocks( $dbw );
@@ -281,8 +283,9 @@ class MergeUser {
 							$options
 						);
 					}
-					$lbFactory->commitMasterChanges( __METHOD__ );
-					wfWaitForSlaves( $checkSince, false, '*' );
+					// Wait for replication to catch up
+					$opts = [ 'ifWritesSince' => $checkSince ];
+					$lbFactory->commitAndWaitForReplication( __METHOD__, $ticket, $opts );
 				} while ( count( $keyValues ) >= $limit );
 			}
 		}
