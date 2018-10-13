8d143a30b11fff7d557f2d74d736b047595846fd
Matěj Suchánek <matejsuchanek97@gmail.com>
2018-10-13 11:48:58 +0200

    Add convenience links for merging users to Special:Contributions
    Bug: T72839
    Change-Id: I7764d21d3fe4967d8ce0c06449ed32f72dd67cf0

--- includes/UserMergeHooks.php
+++ includes/UserMergeHooks.php
@@ -18,4 +18,48 @@ class UserMergeHooks {
 			$reservedUsernames[] = 'Anonymous';
 		}
 	}
+
+	/**
+	 * Handler for ContributionsToolLinks hook
+	 *
+	 * @param int $id
+	 * @param Title $nt
+	 * @param array &$tools
+	 * @param SpecialPage $sp for context
+	 */
+	public static function onContributionsToolLinks(
+		int $id, Title $nt, array &$tools, SpecialPage $sp
+	) {
+		if ( $id === 0 || $id === $sp->getUser()->getId() ) {
+			return;
+		}
+		if ( !$sp->getAuthority()->isAllowed( 'usermerge' ) ) {
+			return;
+		}
+		$targetUser = User::newFromId( $id );
+		if ( array_intersect(
+			$targetUser->getGroups(),
+			$sp->getConfig()->get( 'UserMergeProtectedGroups' )
+		) ) {
+			return;
+		}
+
+		$username = $nt->getText();
+		$linkRenderer = $sp->getLinkRenderer();
+		$tools['usermerge-merge'] = $linkRenderer->makeKnownLink(
+			SpecialPage::getTitleFor( 'UserMerge' ),
+			$sp->msg( 'usermerge-merge-linkoncontribs', $username )->text(),
+			[],
+			[ 'wpolduser' => $username ]
+		);
+		if ( $sp->getConfig()->get( 'UserMergeEnableDelete' ) ) {
+			$tools['usermerge-delete'] = $linkRenderer->makeKnownLink(
+				SpecialPage::getTitleFor( 'UserMerge' ),
+				$sp->msg( 'usermerge-delete-linkoncontribs', $username )->text(),
+				[],
+				[ 'wpolduser' => $username, 'wpdelete' => '1' ]
+			);
+		}
+	}
+
 }
